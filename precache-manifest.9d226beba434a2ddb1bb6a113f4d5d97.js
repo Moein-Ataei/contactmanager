98e065157a0bff672aa78412c1e239b890f6ad05
moein-ataei <moein.ataei@gmail.com>
2020-01-29 14:36:16 +0330

    Updates

---
+++ precache-manifest.9d226beba434a2ddb1bb6a113f4d5d97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a9c0d3d3a3d2210963c61ec9d5e33b",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "3f0da1a3d9d3bd93039a",
+    "url": "/contactmanager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8ab24e25040051d5f40f",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "3f0da1a3d9d3bd93039a",
+    "url": "/contactmanager/static/js/2.85402c25.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.85402c25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ab24e25040051d5f40f",
+    "url": "/contactmanager/static/js/main.d6d1ee73.chunk.js"
+  },
+  {
+    "revision": "d86057960233a7428d99",
+    "url": "/contactmanager/static/js/runtime-main.2ce66dc9.js"
+  }
+]);
